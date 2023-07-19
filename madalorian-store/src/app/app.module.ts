18b8fe4c1b70754e446ea74e76d3b52535b36522
reotsugua <renanaugusto.ras@gmail.com>
2023-07-19 19:31:13 -0300

    iniciando

--- madalorian-store/src/app/app.module.ts
+++ madalorian-store/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
